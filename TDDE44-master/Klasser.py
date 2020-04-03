9773fbaf703d2484cac86de998814e9276feefb8
emisa182 <55809352+emisa182@users.noreply.github.com>
2020-04-03 12:49:16 +0200

    Update Klasser.py

--- TDDE44-master/Klasser.py
+++ TDDE44-master/Klasser.py
@@ -5,13 +5,24 @@ class Pet(object):
 
     def __init__(self, name = ""):
         self.name = name
-        self.kind = kind
+        self.kind = ""
         self.toys = []
 
-    def add_toy(toy)    
+    def add_toy(self, toy):
+        if toy in self.toys:
+            pass
+        else:
+            self.toys.append(toy)
 
-    def __str__():
-
-
-
-dog = Pet()
+    def __str__(self):
+        toy_line = ""
+        if self.toys == []:
+            frase = "{} är en {} som inte har några leksaker."
+        else:
+            frase = "{} är en {} som har följande leksaker: {}"
+            for element in self.toys:
+                if element == self.toys[-1]:
+                    toy_line = toy_line + element + "."
+                else:
+                    toy_line = toy_line + element + ", "
+        return frase.format(self.name, self.kind, toy_line)
