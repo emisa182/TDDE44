766f59b341fd8df7864651b8e5bc08bdd0bf7508
emisa182 <55809352+emisa182@users.noreply.github.com>
2020-04-03 14:53:02 +0200

    Hej

--- TDDE44-master/Lab_5_2.py
+++ TDDE44-master/Lab_5_2.py
@@ -1,3 +1,31 @@
 #!/usr/bin/env python3
+import random
+import Klasser
+Vector2D = Klasser.Vector2D
 
-class Vector2D(object):
+#def test_method():
+
+
+v1 = Vector2D()
+v2 = Vector2D()
+v1.x = random.uniform(-100,100)
+print(v1.x)
+v1.y = random.uniform(-100,100)
+v2.x = random.uniform(-100,100)
+v2.y = random.uniform(-100,100)
+print(v1.get_length())
+v2.add(v1)
+print(v2.x)
+print(v2.y)
+v3 = v2.add_to_new(v1)
+print(v3.x)
+print(v3.y)
+print(v1.is_longer_than(v2))
+v4 = v1.create_unit_vector()
+print(v4.x)
+print(v4.y)
+print(v4.get_length())
+print(str(v2))
+
+
+#lista = [add, add_to_new, create_unit_vector]
