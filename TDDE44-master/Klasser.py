d5f8dc544d183f114b504a588d2a96f0d5fb92e1
emisa182 <55809352+emisa182@users.noreply.github.com>
2020-04-03 10:57:29 +0200

    Create Klasser.py

---
+++ TDDE44-master/Klasser.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+class Pet(object):
+
+
+    def __init__(self, name = ""):
+        self.name = name
+        self.kind = kind
+        self.toys = []
+
+    def add_toy(toy)    
+
+    def __str__():
+
+
+
+dog = Pet()
