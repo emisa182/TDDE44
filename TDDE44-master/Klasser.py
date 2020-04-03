6272753009760fa9f9031f9241378078ee4528b9
emisa182 <55809352+emisa182@users.noreply.github.com>
2020-04-03 16:03:06 +0200

    KLart

--- TDDE44-master/Klasser.py
+++ TDDE44-master/Klasser.py
@@ -36,31 +36,26 @@ class Vector2D(object):
         self.y = y  #float
 
     def get_length(self):
-        print("Running: get_length")
         return ((self.x**2 + self.y**2)**(1/2))
 
     def add(self, v1):
-        print("Running: add")
         self.x = self.x + v1.x
         self.y = self.y + v1.y
         return self.x, self.y
 
     def add_to_new(self, v1):
-        print("Running: add_to_new")
         new_vector = Vector2D()
         new_vector.x = self.x + v1.x
         new_vector.y = self.y + v1.y
         return new_vector
 
     def is_longer_than(self, v1):
-        print("Running: is_longer_than")
         if self.get_length() < v1.get_length():
             return False
         else:
             return True
 
     def create_unit_vector(self):
-        print("Running: create_unit_vector")
         new_vector = Vector2D()
         new_vector.y = self.y * ((self.x**2 + self.y**2)**(-1/2))
         new_vector.x = self.x * ((self.x**2 + self.y**2)**(-1/2))
--- TDDE44-master/Lab_5_2.py
+++ TDDE44-master/Lab_5_2.py
@@ -5,27 +5,80 @@ Vector2D = Klasser.Vector2D
 
 #def test_method():
 
+"""Tilldelning av diverse variabler"""
 
 v1 = Vector2D()
 v2 = Vector2D()
 v1.x = random.uniform(-100,100)
-print(v1.x)
 v1.y = random.uniform(-100,100)
 v2.x = random.uniform(-100,100)
 v2.y = random.uniform(-100,100)
-print(v1.get_length())
-v2.add(v1)
-print(v2.x)
-print(v2.y)
-v3 = v2.add_to_new(v1)
-print(v3.x)
-print(v3.y)
-print(v1.is_longer_than(v2))
-v4 = v1.create_unit_vector()
-print(v4.x)
-print(v4.y)
-print(v4.get_length())
-print(str(v2))
+
+after = "Resultat: {}"
 
 
-#lista = [add, add_to_new, create_unit_vector]
+"""Test av Vector2D"""
+
+print("\n")
+
+def test_get_length(vector):
+    print("Running: get_length")
+    before = "Instansens innehåll innan metoden: Vektorn ({},{})"
+    print(before.format(vector.x, vector.y))
+    after = "Längd på vektor: {}"
+    print(after.format(vector.get_length()))
+test_get_length(v2)
+
+print("\n")
+
+def test_add(v1, v2):
+    print("Running: add")
+    before = "Vi adderar vektorn ({},{}) till vår vektor ({},{})"
+    print(before.format(v1.x, v1.y, v2.x, v2.y))
+    after = "Nya koordinater på vår vektor: ({},{})"
+    v2.add(v1)
+    print(after.format(v2.x, v2.y))
+    return v2
+test_add(v1, v2)
+
+print("\n")
+
+def test_add_to_new(v1, v2):
+    print("Running: add_to_new")
+    before = "Vi adderar vektorn ({},{}) med vektorn ({},{})"
+    print(before.format(v1.x, v1.y, v2.x, v2.y))
+    after = "Den nya vektorns koordinater: ({},{})"
+    v3 = v2.add_to_new(v1)
+    print(after.format(v3.x, v3.y))
+    return v3
+test_add_to_new(v1, v2)
+
+print("\n")
+
+def test_is_longer_than(v1, v2):
+    print("Running: is_longer_than")
+    before = "Vi jämför om vektorn ({},{}) är längre än vektorn ({},{})"
+    print(before.format(v1.x, v1.y, v2.x, v2.y))
+    after = "Resultat: {}"
+    print(after.format(v1.is_longer_than(v2)))
+
+test_is_longer_than(v1, v2)
+
+print("\n")
+
+def test_create_unit_vector(v1):
+    print("Running: create_unit_vector")
+    before = "Vektorn innan normering: ({},{})"
+    print(before.format(v1.x, v1.y))
+    v4 = v1.create_unit_vector()
+    after = ("Enhetsvektorn som skapades är: ({},{})")
+    print(after.format(v4.x, v4.y))
+    return v4
+test_create_unit_vector(v1)
+
+print("\n")
+
+def test__str__(v1):
+    print("Running: __str__")
+    print(str(v1))
+test__str__(v1)
