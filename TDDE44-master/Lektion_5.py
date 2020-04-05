a37d1775d7498984ead9e4b9cd10ee0a2244bf11
emisa182 <55809352+emisa182@users.noreply.github.com>
2020-04-05 15:45:14 +0200

    Update Lektion_5.py

--- TDDE44-master/Lektion_5.py
+++ TDDE44-master/Lektion_5.py
@@ -1,7 +1,20 @@
 #!/usr/bin/env python3
-import Klasser.Contact
 
-#Contact = Klasser.Contact
+class Contact(object):
+
+    def __init__(self, name):
+        self.name = name
+        self.phone_num = ""
+
+    def __str__(self):
+        citation = "name: {} \nphone number: {}"
+        return citation.format(self.name, self.phone_num)
+
+    #def __str__(self):
+        #return "{}, {}".format(self.name, self.phone_num)
+
+    def append_to_name(self, string_to_append):
+        self.name = self.name + string_to_append
 
 
 c1 = Contact("Emil")
