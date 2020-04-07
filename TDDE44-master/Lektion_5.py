e993dcb40c16c868f644f3b05d10003f0e5132b7
Zyx <benno657@student.liu.se>
2020-04-07 09:29:21 +0200

    pep8

--- TDDE44-master/Lektion_5.py
+++ TDDE44-master/Lektion_5.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+
 class Contact(object):
 
     def __init__(self, name):
@@ -10,8 +11,8 @@ class Contact(object):
         citation = "name: {} \nphone number: {}"
         return citation.format(self.name, self.phone_num)
 
-    #def __str__(self):
-        #return "{}, {}".format(self.name, self.phone_num)
+    # def __str__(self):
+    # return "{}, {}".format(self.name, self.phone_num)
 
     def append_to_name(self, string_to_append):
         self.name = self.name + string_to_append
