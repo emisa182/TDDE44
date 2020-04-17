03f6c0c8e66b7196973e712d2dfe4ada94ab8596
Zyx <benno657@student.liu.se>
2020-04-17 11:20:57 +0200

    höhö

---
+++ TDDE44-master/Lab6/Contact.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+
+class Contact(object):
+
+    def __init__(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name
+
+
+    def __str__():
+        
