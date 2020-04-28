cd5ba88c176987bcb0274ba940f87fef9101be95
Zyx <benno657@student.liu.se>
2020-04-28 11:02:59 +0200

    gfdshgsd

--- TDDE44-master/Lab_5/lab5_del2/del2/Lab_5_uppgift_2.py
+++ TDDE44-master/Lab_5/lab5_del2/del2/Lab_5_uppgift_2.py
@@ -1,12 +1,9 @@
 #!/usr/bin/env python3
 
-import tkinter
 import lab5
-import math
 
 
 def row_layout(squares, frame_height, frame_width):
-
     """Placera ut fyrkanterna i listan squares i rader.
 
     Argument:
@@ -14,23 +11,17 @@ def row_layout(squares, frame_height, frame_width):
     frame_height -- Höjden (int) på den Frame som fyrkanterna ligger i
     frame_width  -- Bredden (int) på den Frame som fyrkanterna ligger i
     """
-
     ypos = 0
     xpos = 0
 
     for square in squares:
-
         square_size = square.winfo_width()
-
         square.place(x=(xpos), y=(ypos))
-
         if (xpos + 10 + square_size) < (frame_width - 10 - square_size):
             xpos += 10 + square_size
-
         elif (ypos + square_size) < (frame_height-10 - square_size):
             xpos = 0
             ypos += 10 + square_size
-
         else:
             break
 
