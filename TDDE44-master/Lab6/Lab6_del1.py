638624f71b374d3d9575c1c3d0f742a8887779b7
emisa182 <55809352+emisa182@users.noreply.github.com>
2020-04-29 12:30:04 +0200

    Update Lab6_del1.py

--- TDDE44-master/Lab6/Lab6_del1.py
+++ TDDE44-master/Lab6/Lab6_del1.py
@@ -14,19 +14,19 @@ class Text(object):
     def __init__(self, value):
         """Deklarera instansvariabler."""
         self.value = value
-        self.sentence_list = self.value.split("\n")
-        del self.sentence_list[-1]              # tar bort överflödigt element.
+        self.sentence_list = self.value.split("\n")         # egen
+        del self.sentence_list[-1]                          # tar bort överflödigt element.
 
     def get_num_sentences(self):
         """Få antalet meningar i texten."""
         return len(self.sentence_list)
 
-    def get_more(self):
+    def get_more(self):                                     # egen
         """Få antalet ord och tecken i texten."""
         words = 0
         chars = 0
         for sentence in self.sentence_list:
-            line = Sentence(sentence)
+            line = Sentence(sentence)                   # Flytta upp i __init__ då listelementen ska vara instanser av Sentence
             words += line.get_num_tokens()
             chars += line.chars_in_sentence()
         return [words, chars]
@@ -50,13 +50,13 @@ class Sentence(object):
     def __init__(self, value):
         """Deklarera instansvariabler."""
         self.value = value
-        self.token_list = self.value.split(" ")
+        self.token_list = self.value.split(" ")         # egen, flytta upp chars hit också
 
     def get_num_tokens(self):
         """Räkna antal ord i en mening."""
         return len(self.token_list)
 
-    def chars_in_sentence(self):
+    def chars_in_sentence(self):                        # egen
         """Räkna antal tecken i en mening."""
         chars = 0
         for token in self.token_list:
